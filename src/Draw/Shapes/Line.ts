6dba202e1b98778fd6a1ea9c02d405b4ac5915a6
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-21 00:46:33 +0100

    Fixed Line, rearranged project structure

--- src/Draw/Shapes/Line.ts
+++ src/Draw/Shapes/Line.ts
@@ -7,35 +7,35 @@ export class Line extends Shape {
   private _to: Vec;
   private _a: number;
   private _b: number;
+  private _cached: boolean = false;
 
-  constructor(from: Vec, to: Vec,) {
+  constructor(from: Vec, to: Vec) {
     super();
-    const lt = to.x >= from.x;
-    this._from = Vec.round(lt ? from : to);
-    this._to = Vec.round(lt ? to : from);
+    this._from = Vec.round(from);
+    this._to = Vec.round(to);
   }
 
   get from(): Vec { return this._from; }
   set from(from: Vec) {
     this._from = Vec.round(from);
-    this._dx = Math.abs(from.x - this._to.x);
-    this._dy = Math.abs(from.y - this._to.y);
-    this._sx = (from.x < this._to.x) ? 1 : -1;
-    this._sy = (from.y < this._to.y) ? 1 : 0;
-    this._err = (this._dx - this._dy);
+    this._cached = false;
   }
 
   get to(): Vec { return this.to; }
   set to(to: Vec) {
     this._to = Vec.round(to);
-    this._dx = Math.abs(this._from.x - to.x);
-    this._dy = Math.abs(this._from.y - to.y);
-    this._sx = (this._from.x < to.x) ? 1 : -1;
-    this._sy = (this._from.y < to.y) ? 1 : 0;
-    this._err = (this._dx - this._dy);
+    this._cached = false;
   }
 
   public draw(gfx: Graphics, val: string | undefined): void {
+    if (!this._cached) {
+      this._dx      = Math.abs(this._from.x - this._to.x);
+      this._dy      = Math.abs(this._from.y - this._to.y);
+      this._sx      = this._from.x < this._to.x ? 1 : -1;
+      this._sy      = this._from.y < this._to.y ? 1 : -1;
+      this._err     = this._dx - this.dy;
+      this._cached  = true;
+    }
     const curPos = this._from.clone();
     let err = this._err;
     while (curPos.x !== this._to.x || curPos.y !== this._to.y) {
