fb72d24c674020eb09eeab2f6aa408cad8083efe
TheNonStopGamer <rickopdekop@gmail.com>
2021-06-01 18:20:03 +0200

    Added CoordTransformer to Game

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,12 +1,15 @@
 import { Drawable } from '../Drawable/Drawable.js';
+import { CoordTransformer } from '../Graphics/CoordTransformer.js';
 import { Graphics } from '../Graphics/Graphics.js';
 import { Vec } from '../Graphics/Vec.js';
 
-class Game {
+export class Game {
 	public readonly gfx: Graphics;
+	private readonly _ct: CoordTransformer;
 
 	constructor(gfx: Graphics) {
 		this.gfx = gfx;
+		this._ct = new CoordTransformer(gfx.width, gfx.height);
 	}
 
 	public draw(drawable: Drawable | Drawable[]): void {
